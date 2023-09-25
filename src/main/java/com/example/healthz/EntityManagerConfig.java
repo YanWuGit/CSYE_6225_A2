a0f1f447cb3c80247edb49cab72491de1b2f164d
Beluga_samsung <2x3tech@gmail.com>
2023-09-25 15:05:13 -0700

    initial

---
+++ src/main/java/com/example/healthz/EntityManagerConfig.java
@@ -0,0 +1,34 @@
+package com.example.healthz;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class EntityManagerConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPackagesToScan("com.example.healthz");
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+
+        return entityManagerFactoryBean;
+    }
+}
---
+++ src/main/java/com/example/healthz/HealthzApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthzApplication.class, args);
+	}
+
+}
